4805b3cd900c5365bd9eca88479eb4ddbe1ac051
Bakatov <bakatov.vadim@mail.ru>
2020-04-22 23:16:05 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -31,41 +31,51 @@ $(function(){
 });
 
 
+$(".slider").slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: false,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1820,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+      
+    },
+    {
+      breakpoint: 1470,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+      
+    },
+    {
+      breakpoint: 1020,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+      }
+      
+    },
+  ]
+});
+
+$('.header__btn-menu').on('click', function(){
+  $('.menu').slideToggle();
+});
+$('.user__btn').on('click', function(){
+  $('.entry').slideToggle();
+});
+
+
   var mixer = mixitup('.prodcuts__wrapper');
 
-  $(".slider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    arrows: false,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1820,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-        
-      },
-      {
-        breakpoint: 1470,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-        
-      },
-      {
-        breakpoint: 1020,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-
-        }
-        
-      },
-    ]
-  });
+  
 
 
  
